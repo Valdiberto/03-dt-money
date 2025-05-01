d69110dd92113817440b2e151b04af16273733fc
Valdiberto <junior-dk555@hotmail.com>
2025-05-01 09:27:41 -0300

    dt-money project rocketseat

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/globa'
+import { Transations } from './pages/Transactions'
+import { TransactionsProvider } from './contexts/TransactionsContext'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <TransactionsProvider>
+        <Transations />
+      </TransactionsProvider>
+    </ThemeProvider>
+  )
+}
